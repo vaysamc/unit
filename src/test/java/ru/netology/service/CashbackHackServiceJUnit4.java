63d7b799d6d32d4ddca4c3854d6b280cc686487a
vaysamc <vasak7503@gmail.com>
2024-06-10 22:33:21 +0300

    JUnit4 tests commit

---
+++ src/test/java/ru/netology/service/CashbackHackServiceJUnit4.java
@@ -0,0 +1,39 @@
+package ru.netology.service;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CashbackHackServiceJUnit4 {
+    @Test
+    public void shouldReturn100IfAmountIs900() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+        int expected = 100;
+
+        int actual = service.remain(amount);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReturn0IfAmountIs1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int expected = 0;
+
+        int actual = service.remain(amount);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReturn900IfAmountIs1100() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1100;
+        int expected = 900;
+
+        int actual = service.remain(amount);
+
+        Assert.assertEquals(expected, actual);
+    }
+}
